25b0839155296213050afc2c0a51428f48bbc69e
Damon <126731021+damon314159@users.noreply.github.com>
2025-05-30 22:05:32 +0100

    Reorder type defs in u.rule

--- u/rule.go
+++ u/rule.go
@@ -4,14 +4,16 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// Rule is a generic validation rule that takes a field state and returns an error if validation fails.
+// It is the fundamental building block of the validation system.
+type Rule[T any] = func(FieldState[T]) error
+
+// Specific rule type aliases:
+
 // Numeric is a constraint that permits any numeric type: integer or float.
 type Numeric interface {
 	constraints.Float | constraints.Integer
 }
-
-// Rule is a generic validation rule that takes a field state and returns an error if validation fails.
-// It is the fundamental building block of the validation system.
-type Rule[T any] = func(FieldState[T]) error
 
 // StringRule is a specialised rule type for string validation.
 type StringRule = Rule[string]
